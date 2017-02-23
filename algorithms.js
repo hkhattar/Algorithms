d4f8a15524ac3ca46a350b88444d8fcbfa49e003
hkhattar <himanshikhatter@gmail.com>
2017-02-22 16:26:01 -0800

    reorder array so that even entries come first

--- algorithms.js
+++ algorithms.js
@@ -3,3 +3,53 @@ for(var i=0;i<10;i++)
 {
 	console.log(i)
 }
+
+//swap function
+function swap(a,b)
+{
+	var temp = a;
+	a = b;
+	b = temp;
+	// console.log('a',a);
+	// console.log('b',b);
+}
+
+var a = 7;
+var b = 8;
+
+swap(a,b);
+
+
+
+array = [2,8,7,4,3,5]
+array = [6,4,9,3,8,7]
+
+//reorder the array so that even entries appear first, solve it without allocating additional storage
+function evenOdd(array)
+{
+	console.log(array)
+	var even = 0;
+	var odd = array.length-1;
+	var i = 0;
+
+	while( even !== odd )
+	{
+		if (array[i]%2 == 0)
+		{
+			even = i;
+			i++;
+		}
+		else
+		{
+			var temp = array[i];
+			array[i] = array[odd];
+			array[odd]=temp;
+			odd--;
+		}
+	}
+	console.log(array)
+}
+
+evenOdd(array);
+
+
